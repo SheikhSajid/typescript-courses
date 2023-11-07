42a1eca54f1984f6b76ba27ee46c8cda3f2a9bef
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2023-11-07 21:14:45 +1100

    Structural vs nominal types

--- packages/notes-ts-fundamentals-v4/src/05-structural-vs-nominal-types.ts
+++ packages/notes-ts-fundamentals-v4/src/05-structural-vs-nominal-types.ts
@@ -1,5 +1,12 @@
 //* Nominal vs Structural
 
+//* Nominal typing
+// Based on the name of the type (java, c#)
+
+//* Structural typing: 
+// Based on the structure of the type (typescript). Can be
+// used to define nominal types.
+
 class Car {
   make: string
   model: string
@@ -14,12 +21,28 @@ class Truck {
   towingCapacity: number
 }
 
+class Vehicle {
+  make: string
+  model: string
+  year: number
+}
+
 const vehicle = {
   make: 'Honda',
   model: 'Accord',
   year: 2017,
 }
 
+//* Class names can be used as types
+function printVehicle(vehicle: Vehicle) {
+  console.log(`${vehicle.make} ${vehicle.model} (${vehicle.year})`)
+}
+
+printVehicle(new Car()) //✔️ Fine
+printVehicle(new Truck()) //✔️ Fine
+printVehicle(vehicle) //✔️ Fine
+
+//* Defining the type STRUCTURE inline
 function printCar(car: {
   make: string
   model: string
@@ -27,10 +50,9 @@ function printCar(car: {
 }) {
   console.log(`${car.make} ${car.model} (${car.year})`)
 }
-/*
-//printCar(new Car()) //✔️ Fine
-//printCar(new Truck()) //✔️ Fine
-//printCar(vehicle) //✔️ Fine
-/**/
+
+printCar(new Car()) //✔️ Fine
+printCar(new Truck()) //✔️ Fine
+printCar(vehicle) //✔️ Fine
 
 export default {}
